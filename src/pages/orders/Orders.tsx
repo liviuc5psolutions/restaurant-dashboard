f49beea12cce58fb4688222a2c6c964b4a9be980
Cojocaru Ionut Liviu <liviuc@5psolutions.eu>
2022-06-24 03:31:18 +0300

    Orders check

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ApiService from "../../services/api";
 import { AppPage } from '../../components';
+import {Col, Row} from 'antd';
+import './Orders.scss';
 
 export const Orders: React.FC = () => {
-  const onOkModal = (table:any) => {
-    ApiService.post(`/table/${table}/order/complete`, {}).then((r) => {
-      // fetchOrders();
+  const [isOrdersFetched, setIsOrderFetched] = useState(false);
+  const [orders, setOrders] = useState<any>(undefined);
+
+  useEffect(()=> {
+    if(!isOrdersFetched) {
+      fetchActiveOrders()
+    }
+
+    // setTimeout(()=> fetchActiveOrders(), 10000);
+  });
+
+  const fetchActiveOrders = () => {
+    ApiService.get(`/orders`).then((r) => {
+      setOrders(r.data);
+      setIsOrderFetched(true);
+    })
+  }
+
+  const orderComplete = (id:any) => {
+    ApiService.post(`/table/${id}/order/complete`,{}).then((r) => {
+      fetchActiveOrders();
     })
   }
 
   return (
-      <AppPage title="Tables">
-        //
+      <AppPage title="Orders">
+        <div className="order">
+          <Row justify="center">
+            {typeof orders === 'object' ? Object.keys(orders).map((el:any, index:any) => (
+              <Col className="card-order" span={20} key={index} onClick={()=>orderComplete(el)}>
+                <div className="title">Masa : {el}</div>
+                {orders[el].map((product:any,index:any) => (
+                  <Row>
+                    <Col>{product.name}</Col>
+                  </Row>
+                ))}
+              </Col>
+            )): orders}
+          </Row>
+        </div>
       </AppPage>
   );
 };
